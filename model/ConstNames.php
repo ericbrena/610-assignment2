3ace92e68af82e69842aec6bb2b002923ab2acdd
Eric Brenander <Eric Brenander>
2017-10-10 18:59:36 +0200

    revising mvc model, added new files to improve clean code

---
+++ model/ConstNames.php
@@ -0,0 +1,24 @@
+<?php
+
+class ConstNames {
+    
+	const url = "http://eb223fz.000webhostapp.com";
+
+    //loginview
+    const login = 'LoginView::Login';
+	const logout = 'LoginView::Logout';
+	const name = 'LoginView::UserName';
+	const savedName = "LoginView::SavedUserName";
+	const password = 'LoginView::Password';
+	const keep = 'LoginView::KeepMeLoggedIn';
+	const messageId = 'LoginView::Message';
+
+    //registerview
+	const registerName = "RegisterView::UserName";
+	const savedRegisteredName = "RegisterView::SavedUserName";
+	const registerPassword = "RegisterView::Password";
+	const registerPasswordRepeat = "RegisterView::PasswordRepeat";
+	const register = "RegisterView::Register";
+	const registerMessageId = "RegisterView::Message";
+
+}
