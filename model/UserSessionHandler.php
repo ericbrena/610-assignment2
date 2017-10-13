92de9e9831b58d5f6a15010aaa34fa920b94a02d
ericbrena <eb223fz@student.lnu.se>
2017-10-13 21:07:49 +0200

    Update UserSessionHandler.php

--- model/UserSessionHandler.php
+++ model/UserSessionHandler.php
@@ -9,12 +9,12 @@ class UserSessionHandler {
         }
         return $_SESSION[$id];
     }
-	public function getInfo($id) {
-		$info = "";
-		if(isset($_SESSION[$id])) {
-			$info = $_SESSION[$id];
-		}
-		return $info;
+     public function getInfo($id) {
+	$info = "";
+	if(isset($_SESSION[$id])) {
+	    $info = $_SESSION[$id];
+	}
+	return $info;
     }
     public function saveInfo($id, $message) {
         $_SESSION[$id] = $message;
